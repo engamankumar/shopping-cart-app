e32551f1570a59938efcfdadf9550584f18288fb
Aman Kumar <engamankumar@gmail.com>
2022-04-29 14:42:31 +0530

    first commit

---
+++ src/store/action/action.ts
@@ -0,0 +1,98 @@
+import {
+    LOGIN,
+    ADDTOCART,
+    LOGOUT,
+    USERS_ERROR,
+    GET_BANNER,
+    REMOVE_QTY,
+    ADD_QTY,
+    SELECT_CATEGORY
+} from "../action.type";
+import axios from 'axios'
+import { Dispatch } from "redux";
+
+export const login = () => {
+    return (dispatch: any) => {
+        dispatch({
+            type: LOGIN,
+            payload: '',
+        })
+    }
+}
+
+export const logout = () => {
+    return {
+        type: LOGOUT,
+        payload: ''
+    }
+}
+
+export const getBanner = () => async (dispatch: any) => {
+    try {
+        const banner = await axios.get(`http://localhost:5000/banners`)
+        const category = await axios.get(`http://localhost:5000/categories`)
+        const products = await axios.get(`http://localhost:5000/products`)
+        const data = {
+            banner: banner.data,
+            category: category.data.filter((d: any) => d.order !== -1),
+            products: products.data
+        }
+        dispatch({
+            type: GET_BANNER,
+            payload: data,
+        })
+    }
+    catch (e) {
+        dispatch({
+            type: USERS_ERROR,
+            payload: e,
+        })
+    }
+
+}
+
+export const addToCart = (id: Number) => async (dispatch: Dispatch<any>) => {
+    try {
+        var config = {
+            method: 'post',
+            url: `http://localhost:5000/addToCart?productID=${id}`,
+            headers: {}
+        };
+        const postProduct = await axios(config);
+        if (postProduct.data.response === "Success") {
+            dispatch({
+                type: ADDTOCART,
+                payload: id
+            })
+        }
+
+    }
+    catch (e) {
+        dispatch({
+            type: USERS_ERROR,
+            payload: e,
+        })
+    }
+
+}
+
+export const removeQty = (id: Number) => {
+    return {
+        type: REMOVE_QTY,
+        payload: id
+    }
+}
+
+export const addQty = (id: Number) => {
+    return {
+        type: ADD_QTY,
+        payload: id
+    }
+}
+
+export const selectCategory = (order: Number) => {
+    return {
+        type: SELECT_CATEGORY,
+        payload: order
+    }
+}
